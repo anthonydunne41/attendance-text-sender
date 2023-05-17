75e8b4e7bbf46f557703d6d2824611153ecbd1f1
Anthony Dunne <dunap001@mymail.unisa.com.au>
2023-05-17 17:15:08 +0930

    add basic sms functionaility

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import csv
-import subprocess
-
-def attendance_text_sender():
-    recipient = "+6149121374"
-    message = "Message Text"
-
-    # Build the AppleScript command as a string
-    applescript = f'tell application "Messages" to send "{message}" to buddy "{recipient}" of service "SMS"'
-
-    # Run the AppleScript command using the osascript module
-    subprocess.call(['osascript', '-e', applescript])
+from pushbullet import Pushbullet
 
 
 
-#  with open("csv/testdata.csv", 'r') as csvfile:
-#     csv_reader = csv.reader(csvfile)
+def send_sms():
 
-#     next(csv_reader)
+    pb = Pushbullet('o.dQ75BMOxjgber6nLxVC7RwxyRTHMBTbs')
 
-#     for row in csv_reader:
-#        first_name = row[3]
-#        ph_number = row[8]
-#        #print(first_name + " " + ph_number)
-#        if row[15] == "Check":
-#           print(first_name + " " + ph_number)
-#           print("txt sent")
-#        if row[3] == '':
-#           break
+    device = pb.devices[1]
+
+
+
+
+
+
+
+# def attendance_text_sender():
+#     with open("csv/testdata.csv", 'r') as csvfile:
+#         csv_reader = csv.reader(csvfile)
+
+#         next(csv_reader)
+
+#         for row in csv_reader:
+#             first_name = row[3]
+#             ph_number = row[8]
+#             state = row[15]
+
+#             print(f"{first_name} {state}")
+#             if row[15] == "Check":
+#                 # print(first_name + " " + ph_number)
+#                 print("txt sent")
+#             if row[3] == '':
+#                 break
 
 
 
 
 if __name__ == "__main__":
-    attendance_text_sender()
+    send_sms()
+
+
+
+
+    #push = pb.push_sms(device, "0401249642", "Python text message!")
+    # if push.active:
+    #     print('SMS sent successfully.')
+    # else:
+    #     print('Failed to send SMS.')
