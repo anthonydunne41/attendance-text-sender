88dcf15cb043b837793b4ffb1314b7cbbc1ec2cb
Anthony Dunne <dunap001@mymail.unisa.com.au>
2023-05-19 12:11:10 +0930

    working first build

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import csv
 import os
 from prettytable import PrettyTable
 from pushbullet import Pushbullet
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def file_selector():
+    root = Tk()
+    root.withdraw()
+    file_path = askopenfilename()
+    return str(file_path)
+
+def welcome_message():
+    print("Auto Texter Script")
+    print("Select your .csv file")
+    enter = input("Press enter to continue")
 
 def send_sms(phone_number, message):
     pb = Pushbullet('o.dQ75BMOxjgber6nLxVC7RwxyRTHMBTbs')
 
     device = pb.devices[1]
     push = pb.push_sms(device, phone_number, message)
-    if push.active:
+    if push.get('active', True):
         print('SMS sent successfully.')
     else:
         print('Failed to send SMS.')
-
 
 def index_table(csvreader):
     first_row = next(csvreader)
@@ -42,6 +54,7 @@ def print_table(csv_reader, first_name_i, phone_number_i, date_i):
             table.add_row([first_name, phone_number, filter])
     print(table)
     print("")
+    return table
 
 def print_filtered_table(csv_reader, send_list, first_name_i, phone_number_i, date_i):
     table = PrettyTable()
@@ -55,10 +68,28 @@ def print_filtered_table(csv_reader, send_list, first_name_i, phone_number_i, da
     print(table)
     print("")
 
+def paragraph():
+    print("Please enter your paragraph. Press Enter three times when you're done:\n")
+    lines = []
+    enter_count = 0
+
+    while enter_count < 3:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            enter_count += 1
+
+    paragraph = "\n".join(lines)
+    print("\nYou entered the following paragraph:\n")
+    print(paragraph)
+    return paragraph
 
 def attendance_text_sender():
-    table = PrettyTable()
-    with open("csv/attendance.csv", 'r') as csvfile:
+    clear_terminal()
+    welcome_message()
+    csv_file = file_selector()
+    with open(csv_file, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
 
         index_table(csv_reader)
@@ -68,12 +99,10 @@ def attendance_text_sender():
         phone_number_index = int(input("Enter index of the Phone Number coloum: "))
         date_index = int(input("Enter index of the date to filter by: "))
 
-        # clear_terminal()
         print_table(csv_reader, first_name_index, phone_number_index, date_index)
         csvfile.seek(0)
 
         filter_value = input('Enter value to filter by. eg. "Check": ')
-        # clear_terminal()
 
         send_list = []
         for row in csv_reader:
@@ -83,17 +112,23 @@ def attendance_text_sender():
         
         print_filtered_table(csv_reader, send_list, first_name_index, phone_number_index, date_index)
 
+    while True:
+        greeting = input("Enter greeting\neg. Hi, Good morning, Good afternoon, etc.\n")
+        message = paragraph()
+        clear_terminal()
+        print(f"{greeting}\n\n{message}")
+        print("")
+        yes = input("Send this message? [y|n] ")
+        if (yes == 'y'):
+            break
+        else:
+            clear_terminal()
 
-        
-
-        
-
-
-
+    for person in send_list:
+        print(f"*sending message to {person[0]}*")
+        message_to_send = f"{greeting} {person[0]} \n\n" + message
+        send_sms(person[1], message_to_send)
+        print("")
 
 if __name__ == "__main__":
     attendance_text_sender()
-
-
-
-
---
+++ nothing.py
@@ -0,0 +1,5 @@
+    # for person in send_list:
+    #     print(f"*sending message to {person[0]}*")
+    #     message_to_send = f"{greeting} {person[0]} \n\n" + message
+    #     print(message_to_send)
+    #     print("")
